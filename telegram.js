20ac9fb8691bd32ec978a3b0b86ee0ec99d57da0
AlterEgoDevel <AlterEgoDevel@gmail.com>
2025-03-04 13:59:47 +0200

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,6 +1,9 @@
 // Код для работы с Telegram Web App
 window.Telegram.WebApp.ready(); // Убедись, что WebApp готово
 
+// Разворачиваем приложение на весь экран
+Telegram.WebApp.expand();
+
 // Получение данных о пользователе
 const user = Telegram.WebApp.initDataUnsafe;
 console.log(user);
